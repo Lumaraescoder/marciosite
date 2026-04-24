8f2c955315bd904b1ffdfec610b5a22972966e47
lu_maraes <50714793+Lumaraescoder@users.noreply.github.com>
2026-04-24 12:13:06 +0100

    asdasd

--- components/homes/heros/Hero7.jsx
+++ components/homes/heros/Hero7.jsx
@@ -88,7 +88,7 @@ export default function Hero7() {
           <Image
             width={1920}
             height={960}
-            src="/img/hero/7/capa1.png"
+            src="/img/hero/7/marciofoto.jpeg"
             alt="background"
             className="w-full h-full object-cover"
             style={{ objectPosition: "center 60%" }}
@@ -98,7 +98,7 @@ export default function Hero7() {
           <Image
             width={768}
             height={1024}
-            src="/img/hero/7/capa2.png"
+            src="/img/hero/7/marciofoto.jpeg"
             alt="background mobile"
             className="w-full h-full object-cover"
           />
